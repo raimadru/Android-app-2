e7ab8d57047540cfe545d50f159eeacd59cc6910
Raimobda <raimonda.drungilaite@gmail.com>
2019-04-01 11:10:31 +0300

    Android app 2

---
+++ Androidapp2/app/src/main/java/com/example/androidapp2/MainActivity.java
@@ -0,0 +1,33 @@
+package com.example.androidapp2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private EditText editText;
+    private Button button;
+    private TextView textView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText = (EditText) findViewById(R.id.editText);
+        button = (Button) findViewById(R.id.button);
+        textView = (TextView) findViewById(R.id.textView);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String name = editText.getText().toString();
+                textView.setText("Your name is " + name);
+            }
+        });
+    }
+}
